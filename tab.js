eed91b0e74963eaa3d96dc38e98f77f8c831f421
Charanjeet kaur <charanjeet.kaur@meritnation.com>
2017-06-09 14:57:46 +0530

    tab file added

---
+++ tab.js
@@ -0,0 +1,79 @@
+// @flow weak
+
+import React, { Component } from 'react';
+import Tabs from 'material-ui/Tabs/Tabs';
+import Tab from 'material-ui/Tabs/Tab';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Checkbox from 'material-ui/Checkbox';
+import SwipeableViews from 'react-swipeable-views';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+
+const styles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    backgroundColor: '#6AC0FF',
+  },
+};
+
+class DemoTabs extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {index:0};
+
+    }
+  handleChangeTabs(value){
+    this.setState({
+      index: value,
+    });
+  };
+
+  handleChangeIndex(index){
+    this.setState({
+      index:index ,
+    });
+  };
+
+  render() {
+    const {
+      index,
+    } = this.state;
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <div>
+          <Tabs value={index}>
+            <Tab label="tab n°1" value={0} onClick={() => this.handleChangeTabs(0)}/>
+            <Tab label="tab n°2" value={1} onClick={() => this.handleChangeTabs(1)} />
+            <Tab label="tab n°3" value={2} onClick={() => this.handleChangeTabs(2)} />
+          </Tabs>
+          <SwipeableViews index={index}  onChangeIndex={(index) => this.handleChangeIndex(index)}>
+            <div style={Object.assign({}, styles.slide, styles.slide1)} >
+              slide n°1
+            </div>
+            <div style={Object.assign({}, styles.slide, styles.slide2)}>
+              slide n°2
+              <br />
+              <br />
+              <Checkbox label="test event propagation" />
+            </div>
+            <div style={Object.assign({}, styles.slide, styles.slide3)}>
+              slide n°3
+            </div>
+          </SwipeableViews>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default DemoTabs;
